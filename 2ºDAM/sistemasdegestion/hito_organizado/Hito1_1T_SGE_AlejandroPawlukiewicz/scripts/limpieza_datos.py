f4126edb8c8b6eb73c46b79f6e4d04638387e63f
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2024-10-26 23:49:12 +0000

    soluciona

--- 2ºDAM/sistemasdegestion/hito_organizado/Hito1_1T_SGE_AlejandroPawlukiewicz/scripts/limpieza_datos.py
+++ 2ºDAM/sistemasdegestion/hito_organizado/Hito1_1T_SGE_AlejandroPawlukiewicz/scripts/limpieza_datos.py
@@ -12,9 +12,8 @@ def reemplazar_nan_por_cero(df):
     return df.fillna(0)
 
 def limpiar_datos_incorrectos(df):
-    for column in df.columns:
-        if df[column].dtype in ['int64', 'float64']:
-            df[column] = df[column].clip(lower=0)  # Reemplaza valores negativos por 0
-        else:
-            df[column] = df[column].astype(str)
+    for column in df.select_dtypes(include=[np.number]).columns:
+        df[column] = df[column].clip(lower=0)  # Reemplaza valores negativos por 0
+    for column in df.select_dtypes(exclude=[np.number]).columns:
+        df[column] = df[column].astype(str)
     return df
