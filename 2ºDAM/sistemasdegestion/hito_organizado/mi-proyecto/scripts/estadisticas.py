65e1c0f4839c26b0387210db7147af94a619a937
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2024-10-26 20:46:04 +0000

    guardado

--- 2ºDAM/sistemasdegestion/hito_organizado/mi-proyecto/scripts/estadisticas.py
+++ 2ºDAM/sistemasdegestion/hito_organizado/mi-proyecto/scripts/estadisticas.py
@@ -1,11 +1,13 @@
+
+
 import pandas as pd
+import numpy as np
 from scipy import stats
 
-# Función para calcular estadísticas (media, varianza, moda) de cada columna del DataFrame
 def calcular_estadisticas(df):
     estadisticas = {}
     for column in df.columns:
-        if pd.api.types.is_numeric_dtype(df[column]):
+        if df[column].dtype in ['int64', 'float64']:
             media = df[column].mean()
             varianza = df[column].var()
             moda = stats.mode(df[column])[0][0]
--- 2ºDAM/sistemasdegestion/hito_organizado/mi-proyecto/scripts/limpieza_datos.py
+++ 2ºDAM/sistemasdegestion/hito_organizado/mi-proyecto/scripts/limpieza_datos.py
@@ -1,28 +1,20 @@
-# mi-proyecto/scripts/limpieza_datos.py
-import pandas as pd
 
-# Función para limpiar el DataFrame eliminando filas y columnas vacías
+
+import pandas as pd
+import numpy as np
+
 def limpiar_dataframe(df):
     df.dropna(how='all', inplace=True)
     df.dropna(axis=1, how='all', inplace=True)
     return df
 
-# Función para reemplazar valores NaN por 0
 def reemplazar_nan_por_cero(df):
     return df.fillna(0)
 
-# Función para limpiar datos incorrectos en el DataFrame
 def limpiar_datos_incorrectos(df):
     for column in df.columns:
         if df[column].dtype in ['int64', 'float64']:
             df[column] = df[column].clip(lower=0)  # Reemplaza valores negativos por 0
         else:
             df[column] = df[column].astype(str)
-    return df
-
-
-def eliminar_filas_invalidas(df):
-    df = df[(df != 0).all(axis=1)]  # Eliminar filas con 0
-    df = df.dropna()  # Eliminar filas con NaN
-    df = df[df.applymap(lambda x: isinstance(x, (int, float)))]  # Eliminar filas con texto
     return df
