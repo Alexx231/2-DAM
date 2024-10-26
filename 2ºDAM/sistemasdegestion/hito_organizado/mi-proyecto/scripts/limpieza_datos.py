2b0777dec605841dfa25c748ca2f04667a9f52ba
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2024-10-26 17:35:11 +0000

    popo

--- 2ºDAM/sistemasdegestion/hito_organizado/mi-proyecto/scripts/limpieza_datos.py
+++ 2ºDAM/sistemasdegestion/hito_organizado/mi-proyecto/scripts/limpieza_datos.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 # Función para limpiar el DataFrame eliminando filas y columnas vacías
 def limpiar_dataframe(df):
-    # Eliminar filas y columnas vacías
     df.dropna(how='all', inplace=True)
     df.dropna(axis=1, how='all', inplace=True)
     return df
@@ -15,9 +14,15 @@ def reemplazar_nan_por_cero(df):
 # Función para limpiar datos incorrectos en el DataFrame
 def limpiar_datos_incorrectos(df):
     for column in df.columns:
-        if pd.api.types.is_numeric_dtype(df[column]):
-            df[column] = pd.to_numeric(df[column], errors='coerce')
-            df[column] = df[column].apply(lambda x: x if x >= 0 else 0)
+        if df[column].dtype in ['int64', 'float64']:
+            df[column] = df[column].clip(lower=0)  # Reemplaza valores negativos por 0
         else:
             df[column] = df[column].astype(str)
+    return df
+
+
+def eliminar_filas_invalidas(df):
+    df = df[(df != 0).all(axis=1)]  # Eliminar filas con 0
+    df = df.dropna()  # Eliminar filas con NaN
+    df = df[df.applymap(lambda x: isinstance(x, (int, float)))]  # Eliminar filas con texto
     return df
--- 2ºDAM/sistemasdegestion/hito_organizado/mi-proyecto/scripts/operaciones.py
+++ 2ºDAM/sistemasdegestion/hito_organizado/mi-proyecto/scripts/operaciones.py
@@ -1,7 +1,9 @@
-# scripts/file_operations.py
 
 def guardar_datos_concatenados(colecciones_datos, archivo_salida, encoding='utf-8'):
     with open(archivo_salida, 'w', encoding=encoding) as file:
         for coleccion in colecciones_datos:
-            for dato in coleccion:
-                file.write(f"{dato}\n")
+            if isinstance(coleccion, (list, tuple, set)):
+                for dato in coleccion:
+                    file.write(f"{dato}\n")
+            else:
+                file.write(f"{coleccion}\n")
