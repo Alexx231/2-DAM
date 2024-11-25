5a32ad7c9b2fe34e8032ae856fc23ee1c08731bd
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2024-11-25 20:26:38 +0000

    xe

--- 2ºDAM/psp/Examen/UD2_A2/src/ejercicios/ActividadDos.java
+++ 2ºDAM/psp/Examen/UD2_A2/src/ejercicios/ActividadDos.java
@@ -5,29 +5,34 @@ public class ActividadDos extends Thread {
     private static volatile boolean carreraTerminada = false;
     private static String ganador = "";
 
+    // Constructor que inicializa el nombre del corredor
     public ActividadDos(String nombre) {
         this.nombre = nombre;
     }
 
     @Override
     public void run() {
+        // Bucle que simula el progreso del corredor en la carrera
         for (int i = 1; i <= 10; i++) {
             if (carreraTerminada) {
-                break;
+                break; // Si la carrera ha terminado, salir del bucle
             }
             System.out.println(nombre + " ha completado " + i * 10 + "% de la carrera.");
             try {
+                // Simular el tiempo que tarda en completar cada tramo de la carrera
                 Thread.sleep((int) (Math.random() * 1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        // Si la carrera no ha terminado, este corredor es el ganador
         if (!carreraTerminada) {
             carreraTerminada = true;
             ganador = nombre;
         }
     }
 
+    // Método estático para obtener el nombre del ganador
     public static String getGanador() {
         return ganador;
     }
--- 2ºDAM/psp/Examen/UD2_A2/src/ejercicios/Main.java
+++ 2ºDAM/psp/Examen/UD2_A2/src/ejercicios/Main.java
@@ -2,15 +2,18 @@ package ejercicios;
 
 public class Main {
     public static void main(String[] args) {
+        // Crear tres instancias de ActividadDos, cada una representando un corredor
         ActividadDos corredor1 = new ActividadDos("Corredor 1");
         ActividadDos corredor2 = new ActividadDos("Corredor 2");
         ActividadDos corredor3 = new ActividadDos("Corredor 3");
 
+        // Iniciar los hilos de los corredores
         corredor1.start();
         corredor2.start();
         corredor3.start();
 
         try {
+            // Esperar a que todos los corredores terminen la carrera
             corredor1.join();
             corredor2.join();
             corredor3.join();
@@ -18,6 +21,7 @@ public class Main {
             e.printStackTrace();
         }
 
+        // Imprimir el nombre del ganador de la carrera
         System.out.println("La carrera ha terminado. El ganador es: " + ActividadDos.getGanador());
     }
 }
