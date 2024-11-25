58583c992004d980711e58e737eb2d5498ca036f
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2024-11-25 20:24:09 +0000

    Añadir comentarios explicativos en ActividadUno: mejorar la legibilidad del código al describir la funcionalidad y manejo de excepciones en el método principal.

--- 2ºDAM/psp/Examen/UD2_A1/src/ejercicios/ActividadUno.java
+++ 2ºDAM/psp/Examen/UD2_A1/src/ejercicios/ActividadUno.java
@@ -1,20 +1,22 @@
-package ejercicios;
+package ejercicios; // Define el paquete al que pertenece esta clase
 
 public class ActividadUno {
 
+    // Método principal que se ejecuta al iniciar el programa
     public static void main(String[] args) {
 
+        // Imprime un mensaje indicando que el hilo principal va a dormir
         System.out.println("El hilo principal va a dormirse durante 1 seg");
 
         try {
-
+            // Hace que el hilo principal duerma durante 1000 milisegundos (1 segundo)
             Thread.sleep(1000);
         } catch (InterruptedException e) {
-
+            // Captura la excepción si el hilo principal es interrumpido durante el sueño
             System.err.println("El hilo principal fue interrumpido.");
         }
 
-
+        // Imprime un mensaje indicando que el hilo principal ha despertado
         System.out.println("El hilo principal ha despertado.");
     }
 }
